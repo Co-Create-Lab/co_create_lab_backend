2739bacebd8f99673668203403400b6b7a16feea
SvjetlanaR <svjetlanarados@gmail.com>
2023-02-21 15:32:20 +0100

    post req

--- controllers/projects.js
+++ controllers/projects.js
@@ -2,9 +2,7 @@ const Project = require("../models/projects");
 const { exists } = require("../models/user");
 const { ErrorResponse } = require("../utils/ErrorResponse");
 
-
 const getAllProjects = async (req, res, next) => {
-
   try {
     const project = await Project.find();
     res.status(200).json(project);
@@ -14,13 +12,11 @@ const getAllProjects = async (req, res, next) => {
 };
 
 const getSortedProjects = async (req, res, next) => {
-  const {
-    createdAt,
-    start_date} = req.query
+  const { createdAt, start_date } = req.query;
 
-    const sort = {}
-    if (createdAt) sort.createdAt = createdAt
-    if (start_date) sort.start_date = start_date
+  const sort = {};
+  if (createdAt) sort.createdAt = createdAt;
+  if (start_date) sort.start_date = start_date;
   try {
     const project = await Project.find({}).sort(sort);
     res.status(200).json(project);
@@ -30,26 +26,19 @@ const getSortedProjects = async (req, res, next) => {
 };
 
 const getFilteredProjects = async (req, res, next) => {
-   const {
-     keyword,
-     categories,
-     location,
-     start_date,
-     tech_stack,
-   } = req.query
+  const { keyword, categories, location, start_date, tech_stack } = req.query;
 
-   const filter = {}
-   if (keyword) filter.$text = { $search: keyword }
-   if(location) filter.location = location
-   if(start_date) filter.start_date = start_date
-   if(categories) filter.categories = {$all: categories.split(',')}
+  const filter = {};
+  if (keyword) filter.$text = { $search: keyword };
+  if (location) filter.location = location;
+  if (start_date) filter.start_date = start_date;
+  if (categories) filter.categories = { $all: categories.split(",") };
 
-   console.log(filter)
+  console.log(filter);
 
   try {
     const project = await Project.find(filter);
     res.status(200).json(project);
-    
   } catch (error) {
     next(error);
   }
@@ -58,7 +47,7 @@ const getFilteredProjects = async (req, res, next) => {
 const getOneProject = async (req, res, next) => {
   const { id } = req.params;
   try {
-    const project = await Project.findById(id);
+    const project = await Project.findById(id).populate("creator");
     res.json(project);
   } catch (error) {
     next(error);
@@ -84,7 +73,7 @@ const createProject = async (req, res, next) => {
     start_date,
     tech_stack,
   } = req.body;
-
+  const creator = req.user.id;
   try {
     const project = await Project.create({
       project_name,
@@ -93,6 +82,7 @@ const createProject = async (req, res, next) => {
       location,
       start_date,
       tech_stack,
+      creator,
     });
     res.status(201).json(project);
   } catch (error) {
@@ -132,5 +122,5 @@ module.exports = {
   deleteProject,
   updateProject,
   getFilteredProjects,
-  getSortedProjects
+  getSortedProjects,
 };
